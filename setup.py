88b999be3048d9b936421a8698e9cc0f7af154a3
Your Name <you@example.com>
2024-09-21 09:46:12 +0800

    rename

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="μTask",
+    name="mtask",
     version="0.1.0",
-    description="A simple task queue and scheduler library using Redis and asyncio with greenlet-based workers.",
+    description="μTask is a simple task queue and scheduler library using Redis and asyncio with greenlet-based workers.",
     author="Your Name",
     author_email="your.email@example.com",
-    url="https://github.com/07h/mTask",
+    url="https://github.com/07h/mtask",
     packages=find_packages(),
     install_requires=[
         "aioredis>=2.0.0",
